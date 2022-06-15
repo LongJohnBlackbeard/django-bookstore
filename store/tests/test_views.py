409d0e9f53ebcbe58abadfd594ec4f726a7f0af2
Daniel <72884152+LongJohnBlackbeard@users.noreply.github.com>
2022-06-15 08:24:11 -0500

    Basket Feature finished, tested with coverage, flake8, isot

--- store/tests/test_views.py
+++ store/tests/test_views.py
@@ -1,12 +1,15 @@
+from importlib import import_module
 from unittest import skip
 
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.http import HttpRequest
-from django.test import Client, RequestFactory, TestCase
+from django.test import Client, TestCase
 from django.urls import reverse
 
 from store.models import Category, Product
 from store.views import product_all
+
 
 @skip("demonstrating skipping")
 class TestSkip(TestCase):
@@ -17,7 +20,6 @@ class TestSkip(TestCase):
 class TestViewResponses(TestCase):
     def setUp(self):
         self.c = Client()
-        self.factory = RequestFactory()
         Category.objects.create(name='django', slug='django')
         User.objects.create(username='admin')
         self.data1 = Product.objects.create(category_id=1, title='django beginners', created_by_id=1,
@@ -43,15 +45,8 @@ class TestViewResponses(TestCase):
 
     def test_homepage_url(self):
         request = HttpRequest()
-        response = product_all(request)
-        html = response.content.decode('utf8')
-        self.assertIn('<title>BookStore</title>', html)
-        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_functions(self):
-        """Example: Using request factory"""
-        request = self.factory.get('/item/django-beginners')
+        engine = import_module(settings.SESSION_ENGINE)
+        request.session = engine.SessionStore()
         response = product_all(request)
         html = response.content.decode('utf8')
         self.assertIn('<title>BookStore</title>', html)
