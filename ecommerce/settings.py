ea4013d260ea45e99aa40a3eca0653536677478b
Daniel <72884152+LongJohnBlackbeard@users.noreply.github.com>
2022-08-01 22:51:51 -0500

    changing acl back to read-only

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -113,7 +113,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 AWS_STORAGE_BUCKET_NAME = os.environ["AWS_STORAGE_BUCKET_NAME"]
-AWS_DEFAULT_ACL = "none"
+AWS_DEFAULT_ACL = "read-only"
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
 # s3 static settings
