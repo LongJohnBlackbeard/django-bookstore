417210b06f6b1fee0f6622a74e19335699317bd8
Daniel <72884152+LongJohnBlackbeard@users.noreply.github.com>
2022-08-01 23:26:35 -0500

    type in custom storage file

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -120,7 +120,7 @@ AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
 AWS_LOCATION = "static"
 STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-DEFAULT_FILE_STORAGE = "core.storages.MediaStore"
+DEFAULT_FILE_STORAGE = "core.custom_storages.MediaStore"
 
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
